18a125ca7a5ee1f8656b038527ce5ee90cf50942
Adrian <adrian.pottinger@gmail.com>
2021-05-10 19:16:57 -0400

    homepage dynamic sections

--- components/InstagramApp/InstagramApp.js
+++ components/InstagramApp/InstagramApp.js
@@ -2,16 +2,15 @@
 
   window.addEventListener('DOMContentLoaded', () => {
 
-  $('.imgcontentslider').slick({
-     dots: true,
+  $('.instaslider').slick({
+     dots: false,
      infinite: true,
-     speed: 1000,
-     slidesToShow: 1,
+     speed: 500,
+     slidesToShow: 4,
      slidesToScroll: 1,
      autoplay: true,
-     autoplaySpeed: 5000,
-     arrows: true,
-     fade: true,
+     autoplaySpeed: 2000,
+     arrows: false
    });
 
   });
